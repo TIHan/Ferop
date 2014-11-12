64a35d1432fca6b4c05756d08df5695df04c1321
TIHan <lol.tihan@gmail.com>
2014-11-12 12:47:07 -0800

    More comments, yes

--- src/Ferop/ferop.fs
+++ src/Ferop/ferop.fs
@@ -4,18 +4,21 @@ open System
 open System.Runtime.CompilerServices
 
 /// Marks a class to let Ferop know that its containing static methods marked with the
-/// 'Import' attribute and an 'I' call will be compiled with a C/C++, then
+/// 'Import' attribute and an 'C' call will be compiled with a C/C++, then
 /// the methods will be modified to be P/Invoke methods calling into the compiled code
 /// that was in the 'C' call.
 ///
 /// Static methods marked with 'Export' attribute will tell the C/C++ compiler
 /// that the method needs to be called from within C/C++; this is handled
 /// automatically.
+///
+/// Ferop will be able to determine to compile 32-bit or 64-bit C/C++ based on what
+/// architecture is defined by the assembly.
 [<AttributeUsageAttribute (AttributeTargets.Class)>]
 type FeropAttribute () =
     inherit Attribute ()
 
-/// Allows a hook into the clang command line arguments when compiling
+/// Marks a class to allow a hook into the clang command line arguments when compiling
 /// C/C++ on OSX. The two hooks are {flags} and {libs}.
 ///
 /// When compiling pure C, Ferop will add '-std=c99' to the flags by default to support <stdint.h>.
@@ -30,14 +33,28 @@ type FeropAttribute () =
 type ClangOsxAttribute (flags: string, libs: string) =
     inherit Attribute ()
 
-/// Allows a hook into the gcc/g++ command line arguments when compiling
+/// Marks a class to allow a hook into the gcc/g++ command line arguments when compiling
 /// C/C++ on Linux. The two hooks are {flags} and {libs}.
-/// TODO:
+///
+/// When compiling pure C, Ferop will add '-std=c99' to the flags by default to support <stdint.h>.
+/// 
+/// 32-bit C:
+///     gcc -Wall -m32 -std=c99 -fPIC {flags} -c {cFile} -o {oFile}
+/// 32-bit C++:
+///     g++ -Wall -m32 -fPIC {flags} -c {cFile} -o {oFile}
+/// 32-bit Dynamic Library:
+///     gcc -m32 -fPIC {oFile} -shared -o {soName} {libs}
+/// 64-bit C:
+///     gcc -Wall -m64 -std=c99 -fPIC {flags} -c {cFile} -o {oFile}
+/// 64-bit C++:
+///     g++ -Wall -m64 -fPIC {flags} -c {cFile} -o {oFile}
+/// 64-bit Dynamic Library:
+///     g++ -m64 -fPIC {oFile} -shared -o {soName} {libs}
 [<AttributeUsageAttribute (AttributeTargets.Class)>]
 type GccLinuxAttribute (flags: string, libs: string) =
     inherit Attribute ()
 
-/// Allows a hook into the MSVC command line arguments when compiling
+/// Marks a class to allow a hook into the MSVC command line arguments when compiling
 /// C/C++ on Windows. The hook is {options}.
 ///
 /// Ferop will try to choose the very latest MSVC version based on what is stored in
@@ -47,7 +64,7 @@ type GccLinuxAttribute (flags: string, libs: string) =
 /// MSVC needs to support <stdint.h> in order to compile.
 ///
 /// 32-bit / 64-bit C/C++:
-///     cl.exe {cfile} {options} /link /DLL /OUT:{dllName}
+///     cl.exe {cFile} {options} /link /DLL /OUT:{dllName}
 [<AttributeUsageAttribute (AttributeTargets.Class)>]
 type MsvcWinAttribute (options: string) =
     inherit Attribute ()
