f699912bb991f06ca6ebb5ba7e256e100b67e865
Will Smith <lol.tihan@gmail.com>
2014-12-09 00:22:04 -0800

    beta2 release

--- src/Ferop/AssemblyInfo.fs
+++ src/Ferop/AssemblyInfo.fs
@@ -4,6 +4,6 @@ open System.Reflection
 [<assembly: AssemblyTitleAttribute("Ferop")>]
 [<assembly: AssemblyProductAttribute("Ferop")>]
 [<assembly: AssemblyDescriptionAttribute("Ferop is a .NET library that allows inline C/C++ to compile and run on Windows/Linux/OSX.")>]
-[<assembly: AssemblyVersionAttribute("0.9.0.0")>]
-[<assembly: AssemblyFileVersionAttribute("0.9.0.0")>]
+[<assembly: AssemblyVersionAttribute("1.0.0.0-beta2")>]
+[<assembly: AssemblyFileVersionAttribute("1.0.0.0-beta2")>]
 do ()
