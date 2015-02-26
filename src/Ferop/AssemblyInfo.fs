1f2c9c82473249dc68e78ae924e382a57c45f462
Will Smith <lol.tihan@gmail.com>
2015-02-25 22:58:11 -0800

    Added nuspec and ferop icon

--- src/Ferop/AssemblyInfo.fs
+++ src/Ferop/AssemblyInfo.fs
@@ -4,6 +4,6 @@ open System.Reflection
 [<assembly: AssemblyTitleAttribute("Ferop")>]
 [<assembly: AssemblyProductAttribute("Ferop")>]
 [<assembly: AssemblyDescriptionAttribute("Ferop is a .NET library that allows inline C/C++ to compile and run on Windows/Linux/OSX.")>]
-[<assembly: AssemblyVersionAttribute("1.0.0-beta5")>]
-[<assembly: AssemblyFileVersionAttribute("1.0.0-beta5")>]
+[<assembly: AssemblyVersionAttribute("1.0.0-beta6")>]
+[<assembly: AssemblyFileVersionAttribute("1.0.0-beta6")>]
 do ()
